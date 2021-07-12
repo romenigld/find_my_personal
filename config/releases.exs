48ad2d41ef0179e6b12b2b91171af32b5264ba06
Romenig Lima Damasio <romenigld@gmail.com>
2021-07-12 17:21:25 +0200

    quit commits on releases

--- config/releases.exs
+++ config/releases.exs
@@ -30,12 +30,4 @@ config :find_my_personal, FindMyPersonalWeb.Endpoint,
   ],
   secret_key_base: secret_key_base
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
 config :find_my_personal, FindMyPersonalWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
