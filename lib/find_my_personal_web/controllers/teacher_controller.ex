70e97d4222857d8571a7567895b7f3a49c6c38aa
Romenig Lima Damasio <romenigld@gmail.com>
2021-07-28 01:10:27 +0200

    3 - Perfomance: Recursos Avancados Phoenix - Revisao HTML com o Phoenix

--- lib/find_my_personal_web/controllers/teacher_controller.ex
+++ lib/find_my_personal_web/controllers/teacher_controller.ex
@@ -2,6 +2,7 @@ defmodule FindMyPersonalWeb.TeacherController do
   use FindMyPersonalWeb, :controller
 
   alias FindMyPersonal.Teachers
+  alias FindMyPersonal.Teachers.Teacher
 
   def index(conn, _params) do
     teachers = Teachers.list_teachers()
@@ -11,5 +12,51 @@ defmodule FindMyPersonalWeb.TeacherController do
   def show(conn, %{"id" => id}) do
     teacher = Teachers.get_teacher!(id)
     render(conn, "show.html", teacher: teacher)
+  end
+
+  def new(conn, _params) do
+    changeset = Teachers.change_teacher(%Teacher{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"teacher" => teacher_params}) do
+    case Teachers.create_teacher(teacher_params) do
+      {:ok, teacher} ->
+        conn
+        |> put_flash(:info, "Teacher created successfully.")
+        |> redirect(to: Routes.teacher_path(conn, :show, teacher))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => id}) do
+    teacher = Teachers.get_teacher!(id)
+    changeset = Teachers.change_teacher(teacher)
+    render(conn, "edit.html", teacher: teacher, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "teacher" => teacher_params}) do
+    teacher = Teachers.get_teacher!(id)
+
+    case Teachers.update_teacher(teacher, teacher_params) do
+      {:ok, teacher} ->
+        conn
+        |> put_flash(:info, "Teacher updated successfully.")
+        |> redirect(to: Routes.teacher_path(conn, :show, teacher))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", teacher: teacher, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    teacher = Teachers.get_teacher!(id)
+    {:ok, _teacher} = Teachers.delete_teacher(teacher)
+
+    conn
+    |> put_flash(:info, "Teacher deleted successfully.")
+    |> redirect(to: Routes.teacher_path(conn, :index))
   end
 end
